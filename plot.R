b8d15d4c3720e13e48096f27638989ec528cdff8
Shantanu Goyal <59186786+shantanu2307@users.noreply.github.com>
2022-04-29 17:31:33 +0530

    Update plot.R

--- plot.R
+++ plot.R
@@ -4,5 +4,6 @@ data <- data.frame(
   layers=c("1536","512","256","128","64"),  
   val=c(93.4, 93.6,93.8,94.2,93.5)
 )
-ggplot(data, aes(x=layers, y=val)) + 
-  geom_bar(stat = "identity")
+ggplot(data, aes(x = factor(layers, level = c("1536","512","256","128","64")), y=val)) + 
+  geom_bar(stat = "identity")+
+  geom_text(aes(label=val), vjust=1.6, color="white", size=3.5)
